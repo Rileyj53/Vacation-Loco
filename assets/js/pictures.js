cccacbecd13a43f16bc771620295407a1383d14b
Riley Jacobson <98298991+Rileyj53@users.noreply.github.com>
2023-02-13 17:18:19 -0500

    working on the js

---
+++ assets/js/pictures.js
@@ -0,0 +1,52 @@
+export const pictures = [
+    {
+        imgUrl: 'url(assets/img/0.webp)',
+        // Photo from unsplash.com
+        // Photographer: Cosmic Timetraveler
+        imgLocation: 'Costa Rica',
+        slogan: 'Discover the Pura Vida of Costa Rica'
+    },
+    {
+        imgUrl: 'url(assets/img/1.webp)',
+        // Photo from unsplash.com
+        // Photographer: Angel Silva
+        imgLocation: 'Panama',
+        slogan: 'Experience the Beauty and Adventure of Panama'
+    },
+    {
+        imgUrl: 'url(assets/img/2.webp)',
+        // Photo from unsplash.com
+        // Photographer: Yi Wei
+        imgLocation: 'Dominican Republic',
+        slogan: 'Escape to the Sun-Soaked Beaches of the Dominican Republic'
+    },
+    {
+        imgUrl: 'url(assets/img/3.webp)',
+        // Photo from unsplash.com
+        // Photographer: Raphael Nogueira
+        imgLocation: 'Brazil',
+        slogan: 'Uncover the Vibrant Culture of Brazil'
+    },
+    {
+        imgUrl: 'url(assets/img/4.webp)',
+        // Photo from unsplash.com
+        // Photographer: Robinson Recalde
+        imgLocation: 'Ecuador',
+        slogan: 'Explore the Wonders of Ecuador'
+    },
+    {
+        imgUrl: 'url(assets/img/5.webp)',
+        // Photo from unsplash.com
+        // Photographer: Kiril Georgiev
+        imgLocation: 'Aruba',
+        slogan: 'Relax and Recharge on the Beaches of Aruba'
+    },
+    {
+        imgUrl: 'url(assets/img/6.webp)',
+        // Photo from unsplash.com
+        // Photographer: Saul Mercado
+        imgLocation: 'Colombia',
+        slogan: 'Embrace the Beauty and Diversity of Colombia'
+    },
+];
+
