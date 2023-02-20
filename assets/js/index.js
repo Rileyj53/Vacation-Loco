9ffe6ba503ed1c638a533bc4f008d43ce98acb2e
Riley Jacobson <98298991+Rileyj53@users.noreply.github.com>
2023-02-19 21:06:12 -0500

    working on the form

--- assets/js/index.js
+++ assets/js/index.js
@@ -216,3 +216,22 @@ const imageArrays = {
     // aruba: arubaImg,
     // colombia: colombiaImg,
 };
+
+//form function test
+function formTest() {
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+// let phone = document.getElementById('phone');
+    let radio = document.getElementById('radio');
+    let people = document.getElementById('people');
+    let days = document.getElementById('days');
+
+    alert(`Thank you ${name.value} for your inquiry about a trip to ${radio.value} for ${people.value} people and ${days.value} nights. We will be in touch with you shortly at ${email.value} with more information about your trip.`)
+
+    // change the css for the class success-message from display: none to display: block
+    // document.querySelector('.success-message').style.display = 'block';
+    // document.querySelector('.contact-form').style.display = 'none';
+    // document.getElementById('success1').innerHTML = `Thank you ${name.value} for your inquiry about a trip to ${radio.value} for ${people.value} people and ${days.value} nights.`;
+    // document.getElementById('success2').innerHTML = `We will be in touch with you shortly at ${email.value} with more information about your trip.`;
+
+}
